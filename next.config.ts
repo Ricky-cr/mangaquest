10ca2f5ee8e7190443deeec542514fe1bf43b90d
Riccardo <ricky@MacBook-Air-di-Riccardo-2.local>
2025-08-15 00:25:32 +0200

    New Start

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from 'next';
-import withFlowbiteReact from 'flowbite-react/plugin/nextjs';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withFlowbiteReact(nextConfig);
+export default nextConfig;
