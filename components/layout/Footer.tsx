cd287c214d68db7cd4012a5cb1207e101eb4ec6b
Riccardo <ricky@Mac-4.homenet.telecomitalia.it>
2025-11-29 19:41:55 +0100

    restart project

--- components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-white/10 mt-8 py-6 text-sm text-white/60">
-      <div className="mx-auto max-w-6xl px-4">MangaQuest © 2025</div>
-    </footer>
-  );
-}
